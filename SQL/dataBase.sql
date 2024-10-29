2fd919c185227c522c21b7c68d099c2a4e68258b
MrTheCats <153516781+TTVTheCats@users.noreply.github.com>
2024-10-29 23:12:44 +0100

    Flexbox only version (Testing)

--- SQL/dataBase.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30),
-    email VARCHAR(40),
-    password VARCHAR(255), /* 255 Necessario per contenere la password criptata*/
-    agree_terms BOOLEAN
-);
--- html/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();  // Avvia la sessione
-session_unset();  // Rimuovi tutte le variabili di sessione
-session_destroy();  // Distruggi la sessione
-header("Location: loginPage.php");  // Reindirizza al login
-exit();
-?>
